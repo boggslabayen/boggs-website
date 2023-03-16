7d0af788c13f7a640781f854c663039e271da0b0
Boggs Labayen <boggslabayen@Boggss-Mac-mini.local>
2023-03-16 22:42:49 +0800

    Complete Home Page Portfolio Website

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const ejs = require("ejs");
+
+app.use (express.static("public"));
+app.set ("view engine", "ejs");
+
+app.get("/", function(req,res){
+    res.render("home");
+});
+
+app.get("/resume", function(req, res){
+    res.render("resume");
+})
+
+
+app.listen(3000, function(){
+    console.log("started @ 3000")
+  });
